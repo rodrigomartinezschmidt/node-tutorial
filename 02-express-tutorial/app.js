ca48e52457b20299d183cae9400ea125a4117048
Rodrigo Martinez Schmidt <rodrigomartinez.schmidt@gmail.com>
2021-04-28 21:24:19 -0300

    second commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+app.listen(5000, (req, res) => {
+  console.log('server is listening on port 5000...')
+})
